7fa4a32e7ea6649c4bec48620d6afee05a928cf0
samettyldrm <smtyldrm32@gmail.com>
2024-04-28 02:57:43 +0200

    footer-color

--- src/app/footer.js
+++ src/app/footer.js
@@ -6,10 +6,10 @@ const greatvibes = Great_Vibes({ subsets: ["latin"], weight: ["400"] });
 
 export default function Footer() {
   return (
-    <div className="w-full bottom-0 py-2 bg-background grid grid-cols-4 gap-10 p-10 px-40  justify-center items-center text-center ">
-      <div className="flex flex-col justify-center items-center">
+    <div className="w-full bottom-0 py-2 bg-background grid grid-cols-4 gap-10 p-10 px-40  justify-center text-center text-background-dark bg-background-light  ">
+      <div className="flex flex-col justify-center items-center  ">
         <Image
-          className="w-24"
+          className="w-24 "
           src={"/images/sylogodark.png"}
           width={1920}
           height={1080}
@@ -18,8 +18,8 @@ export default function Footer() {
           samed<span className="font-bold">yıldırım</span>
         </h1>
       </div>
-      <div>
-        <h1 className="font-bold p-2">Links</h1>
+      <div className="items-start ">
+        <h1 className="font-bold p-2 ">Links</h1>
         <div className="w-full border border-background-dark"></div>
         <ul className="p-2">
           <li>About</li>
@@ -29,26 +29,24 @@ export default function Footer() {
           <li>Contact</li>
         </ul>
       </div>
-      <div>
-        <h1 className="font-bold p-2">LynTech Digital</h1>
-        <div className="w-full border border-background-dark"></div>
+      <div className="items-start">
+        <h1 className="font-bold p-2 ">LynTech Digital</h1>
+        <div className="w-full border border-background-dark "></div>
         <ul className="p-2">
           <li>Web Design</li>
           <li>Graphic Design</li>
           <li>Lyn Tech About</li>
-          <li>Certificates & Courses</li>
-          <li>Contact</li>
         </ul>
       </div>
 
       <div className="flex flex-col justify-center items-center gap-2">
         <Image
-          className="w-24 invert grayscale"
+          className="w-24 invert"
           src={"/images/lyntechlogo.png"}
           width={1920}
           height={1080}
         />
-        <h1 >
+        <h1>
           LynTech<span className="font-bold">Digital</span>
         </h1>
       </div>
