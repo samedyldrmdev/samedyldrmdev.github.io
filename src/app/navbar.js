480d60e2292448475eab9835c39aba1b1000711c
samettyldrm <smtyldrm32@gmail.com>
2024-04-29 02:54:28 +0200

    Update navbar.js

--- src/app/navbar.js
+++ src/app/navbar.js
@@ -183,7 +183,7 @@ export default function Navbar({ sections }) {
             onClick={() => {
               showNavbar();
             }}
-            className="flex lg:hidden cursor-pointer"
+            className="flex lg:hidden cursor-pointer text-2xl"
           >
             <FontAwesomeIcon icon={navbarShow ? faBarsStaggered : faBars} />
           </div>
