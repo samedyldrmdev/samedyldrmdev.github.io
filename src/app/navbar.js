bc20c015a5b9a0bffdf69d1727b90ed01b1512aa
samettyldrm <smtyldrm32@gmail.com>
2024-04-29 02:51:55 +0200

    Update navbar.js

--- src/app/navbar.js
+++ src/app/navbar.js
@@ -75,55 +75,66 @@ export default function Navbar({ sections }) {
 
   const [navbarShow, setNavbarShow] = useState(false);
   const showNavbar = () => {
-    
     setNavbarShow(!navbarShow);
   };
 
   return (
     <div>
       <div
-        className={navbarShow ? "fixed h-full w-full bg-background-dark z-40" : "hidden"}
+        className={
+          navbarShow ? "fixed h-full w-full bg-background-dark z-40" : "hidden"
+        }
       >
         <div className="h-full w-full flex flex-col justify-center items-center z-50 text-white">
-            {sections.map((section) => {
-              return (
-                <ul>
-                  <li
-                    key={section.id}
-                    onClick={(e) => {
-                      e.preventDefault();
-                      handleNavLinkClick(section.id, section.name);
-                      showNavbar();
-                    }}
-                    className={clsx(
-                      "hover:animate-pulse max-w-max cursor-pointer text-xl p-2",
-                      {
-                        "text-text-light font-bold":
-                          activeSection === section.id,
-                      }
-                    )}
-                  >
-                    {section.name}
-                  </li>
-                </ul>
-              );
-            })}
-            <div className="absolute bottom-12 gap-8 flex ">
-        <Link href={"https://twitter.com/sametyylldrm"}>
-          <FontAwesomeIcon className="size-5 hover:translate-y-[-3px]" icon={faXTwitter} />
-        </Link>
-        <Link href={"https://www.linkedin.com/in/samedyldrm/"}>
-          <FontAwesomeIcon className="size-5 hover:translate-y-[-3px]" icon={faLinkedinIn} />
-        </Link>
-        <Link href={"https://www.behance.net/samedyldrmm"}>
-          <FontAwesomeIcon className="size-5 hover:translate-y-[-3px]" icon={faBehance} />
-        </Link>
-        <Link href={"https://github.com/samedyldrmdev"}>
-          <FontAwesomeIcon className="size-5 hover:translate-y-[-3px]" icon={faGithub} />
-        </Link>
-      </div>
+          {sections.map((section) => {
+            return (
+              <ul>
+                <li
+                  key={section.id}
+                  onClick={(e) => {
+                    e.preventDefault();
+                    handleNavLinkClick(section.id, section.name);
+                    showNavbar();
+                  }}
+                  className={clsx(
+                    "hover:animate-pulse max-w-max cursor-pointer text-xl p-2",
+                    {
+                      "text-text-light font-bold": activeSection === section.id,
+                    }
+                  )}
+                >
+                  {section.name}
+                </li>
+              </ul>
+            );
+          })}
+          <div className="absolute bottom-12 gap-8 flex ">
+            <Link href={"https://twitter.com/sametyylldrm"}>
+              <FontAwesomeIcon
+                className="size-5 hover:translate-y-[-3px]"
+                icon={faXTwitter}
+              />
+            </Link>
+            <Link href={"https://www.linkedin.com/in/samedyldrm/"}>
+              <FontAwesomeIcon
+                className="size-5 hover:translate-y-[-3px]"
+                icon={faLinkedinIn}
+              />
+            </Link>
+            <Link href={"https://www.behance.net/samedyldrmm"}>
+              <FontAwesomeIcon
+                className="size-5 hover:translate-y-[-3px]"
+                icon={faBehance}
+              />
+            </Link>
+            <Link href={"https://github.com/samedyldrmdev"}>
+              <FontAwesomeIcon
+                className="size-5 hover:translate-y-[-3px]"
+                icon={faGithub}
+              />
+            </Link>
           </div>
-          
+        </div>
       </div>
       <nav
         className={`${navbarFont.className} fixed w-full top-0 py-4 left-0 text-white z-50 bg-background-dark shadow-md shadow-background-less-dark`}
@@ -179,7 +190,7 @@ export default function Navbar({ sections }) {
         </div>
       </nav>
       <div
-        className={`${navbarFont.className} mix-blend-difference bg-black hidden md:block lg:block `}
+        className={`${navbarFont.className} mix-blend-difference bg-black hidden md:block lg:block  `}
       >
         <HeaderTitle name={activeSectionName} />
       </div>
